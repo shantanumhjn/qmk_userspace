d82d1150278a0f32bb1cd66198c0edafb541b92e
Drashna Jael're <drashna@live.com>
2023-09-15 04:11:32 -0700

    Move rgb rules out of user rules

--- users/drashna/rules.mk
+++ users/drashna/rules.mk
@@ -61,28 +61,6 @@ ifeq ($(strip $(CUSTOM_TAP_DANCE)), yes)
     endif
 endif
 
-CUSTOM_RGBLIGHT ?= yes
-ifeq ($(strip $(RGBLIGHT_ENABLE)), yes)
-    ifeq ($(strip $(CUSTOM_RGBLIGHT)), yes)
-        SRC += $(USER_PATH)/rgb/rgb_stuff.c
-        OPT_DEFS += -DCUSTOM_RGBLIGHT
-        ifeq ($(strip $(RGBLIGHT_NOEEPROM)), yes)
-            OPT_DEFS += -DRGBLIGHT_NOEEPROM
-        endif
-        ifeq ($(strip $(RGBLIGHT_STARTUP_ANIMATION)), yes)
-            OPT_DEFS += -DRGBLIGHT_STARTUP_ANIMATION
-        endif
-    endif
-endif
-
-CUSTOM_RGB_MATRIX ?= yes
-ifeq ($(strip $(RGB_MATRIX_ENABLE)), yes)
-    ifeq ($(strip $(CUSTOM_RGB_MATRIX)), yes)
-        SRC += $(USER_PATH)/rgb/rgb_matrix_stuff.c
-        OPT_DEFS += -DCUSTOM_RGB_MATRIX
-        RGB_MATRIX_CUSTOM_USER = yes
-    endif
-endif
 
 KEYLOGGER_ENABLE ?= no
 ifdef CONSOLE_ENABLE
@@ -100,6 +78,7 @@ endif
 -include $(USER_PATH)/pointing/rules.mk
 -include $(USER_PATH)/split/rules.mk
 -include $(USER_PATH)/painter/rules.mk
+-include $(USER_PATH)/rgb/rules.mk
 
 CUSTOM_BOOTMAGIC_ENABLE ?= yes
 ifeq ($(strip $(CUSTOM_BOOTMAGIC_ENABLE)), yes)
