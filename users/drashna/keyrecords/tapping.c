aa4429e12eaa1608353791e2cadf47b10afc9ac3
Drashna Jael're <drashna@live.com>
2023-10-02 12:26:37 -0700

    Fixup and format

--- users/drashna/keyrecords/tapping.c
+++ users/drashna/keyrecords/tapping.c
@@ -92,32 +92,3 @@ bool get_retro_tapping(uint16_t keycode, keyrecord_t *record) {
     }
 }
 #endif // RETRO_TAPPING_PER_KEY
-
-bool achordion_chord(uint16_t tap_hold_keycode, keyrecord_t *tap_hold_record, uint16_t other_keycode, keyrecord_t *other_record) {
-    // Exceptionally consider the following chords as holds, even though they
-    // are on the same hand in Dvorak.
-    switch (tap_hold_keycode) {
-        case QK_LAYER_TAP ... QK_LAYER_TAP_MAX:
-        return true;
-       case HOME_A: // A + U.
-            if (other_keycode == HOME_U) {
-                return true;
-            }
-            break;
-
-        case HOME_S: // S + H and S + G.
-            if (other_keycode == HOME_H || other_keycode == KC_G) {
-                return true;
-            }
-            break;
-    }
-
-    // Also allow same-hand holds when the other key is in the rows below the
-    // alphas. I need the `% (MATRIX_ROWS / 2)` because my keyboard is split.
-    if (other_record->event.key.row % (MATRIX_ROWS / 2) >= 4) {
-        return true;
-    }
-
-    // Otherwise, follow the opposite hands rule.
-    return achordion_opposite_hands(tap_hold_record, other_record);
-}
