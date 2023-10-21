17b5ec1137ada83521a284eb389cef9a22b3f374
Drashna Jael're <drashna@live.com>
2023-10-20 20:13:22 -0700

    Add ADB keyboard convert for friend

---
+++ keyboards/converter/adb_usb/keymaps/awilhite/config.h
@@ -0,0 +1,16 @@
+#pragma once
+
+/* ADB port setting */
+// #define ADB_PORT        PORTD
+// #define ADB_PIN         PIND
+// #define ADB_DDR         DDRD
+// #define ADB_DATA_BIT    0
+// #define ADB_PSW_BIT     1       // optional
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 1
+// #define NO_ACTION_ONESHOT
+// #define NO_ACTION_LAYER
+// #define NO_ACTION_TAPPING
+
+#define BOOTMAGIC_LITE_ROW 6
+#define BOOTMAGIC_LITE_COLUMN 5
