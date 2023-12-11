41f3217d7b8399b55d684164ad49662c51458451
Shantanu Mahajan <shantanu.mahajan@oracle.com>
2023-12-11 13:56:17 +0530

    some refactoring

--- keyboards/sofle/keymaps/shantanumhjn/keymap.c
+++ keyboards/sofle/keymaps/shantanumhjn/keymap.c
@@ -19,50 +19,6 @@
 
 #include QMK_KEYBOARD_H
 
-#define INDICATOR_BRIGHTNESS 30
-#define U_SPLIT_LED_COUNT 36
-
-#define HSV_OVERRIDE_HELP(h, s, v, Override) h, s , Override
-#define HSV_OVERRIDE(hsv, Override) HSV_OVERRIDE_HELP(hsv,Override)
-
-// Light combinations
-#define SET_INDICATORS(hsv) \
-	{0, 1, HSV_OVERRIDE_HELP(hsv, INDICATOR_BRIGHTNESS)}, \
-    {U_SPLIT_LED_COUNT+0, 1, hsv}
-#define SET_UNDERGLOW(hsv) \
-	{1, 6, hsv}, \
-    {U_SPLIT_LED_COUNT+1, 6,hsv}
-#define SET_NUMPAD(hsv)     \
-	{35+15, 5, hsv},\
-	  {35+22, 3, hsv},\
-	  {35+27, 3, hsv}
-#define SET_NUMROW(hsv) \
-	{10, 2, hsv}, \
-		{20, 2, hsv}, \
-		{30, 2, hsv}, \
-	  {35+ 10, 2, hsv}, \
-	  {35+ 20, 2, hsv}, \
-	  {35+ 30, 2, hsv}
-#define SET_INNER_COL(hsv)	\
-	{33, 4, hsv}, \
-	  {35+ 33, 4, hsv}
-
-#define SET_OUTER_COL(hsv) \
-	{7, 4, hsv}, \
-	  {35+ 7, 4, hsv}
-#define SET_THUMB_CLUSTER(hsv) 	\
-	{25, 2, hsv}, \
-	  {35+ 25, 2, hsv}
-#define SET_LAYER_ID(hsv) 	\
-	{0, 1, HSV_OVERRIDE_HELP(hsv, INDICATOR_BRIGHTNESS)}, \
-    {U_SPLIT_LED_COUNT+0, 1, HSV_OVERRIDE_HELP(hsv, INDICATOR_BRIGHTNESS)}, \
-		{1, 6, hsv}, \
-    {U_SPLIT_LED_COUNT+1, 6, hsv}, \
-		{8, 4, hsv}, \
-	  {U_SPLIT_LED_COUNT+8, 4, hsv}, \
-		{26, 2, hsv}, \
-	  {U_SPLIT_LED_COUNT+26, 2, hsv}
-
 
 enum sofle_layers {
     _DEFAULTS = 0,
@@ -315,8 +271,45 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 #ifdef RGBLIGHT_ENABLE
-char layer_state_str[70];
-// Now define the array of layers. Later layers take precedence
+
+#define INDICATOR_BRIGHTNESS 30
+#define U_SPLIT_LED_COUNT 36
+
+#define HSV_OVERRIDE_HELP(h, s, v, Override) h, s , Override
+#define HSV_OVERRIDE(hsv, Override) HSV_OVERRIDE_HELP(hsv,Override)
+
+// Light combinations
+#define SET_INDICATORS(hsv) \
+    {0, 1, HSV_OVERRIDE_HELP(hsv, INDICATOR_BRIGHTNESS)}, \
+    {U_SPLIT_LED_COUNT+0, 1, HSV_OVERRIDE_HELP(hsv, INDICATOR_BRIGHTNESS)}
+#define SET_UNDERGLOW(hsv) \
+    {1, 6, hsv}, \
+    {U_SPLIT_LED_COUNT+1, 6,hsv}
+#define SET_NUMPAD(hsv)     \
+	{35+15, 5, hsv},\
+	  {35+22, 3, hsv},\
+	  {35+27, 3, hsv}
+#define SET_NUMROW(hsv) \
+	{10, 2, hsv}, \
+		{20, 2, hsv}, \
+		{30, 2, hsv}, \
+	  {35+ 10, 2, hsv}, \
+	  {35+ 20, 2, hsv}, \
+	  {35+ 30, 2, hsv}
+#define SET_INNER_COL(hsv)	\
+    {32, 4, hsv}, \
+    {U_SPLIT_LED_COUNT+32, 4, hsv}
+#define SET_OUTER_COL(hsv) \
+    {8, 4, hsv}, \
+    {U_SPLIT_LED_COUNT+8, 4, hsv}
+#define SET_THUMB_CLUSTER(hsv) 	\
+    {26, 2, hsv}, \
+    {U_SPLIT_LED_COUNT+26, 2, hsv}
+#define SET_LAYER_ID(hsv) 	\
+    SET_INDICATORS(hsv), \
+    SET_UNDERGLOW(hsv), \
+    SET_OUTER_COL(hsv), \
+    SET_THUMB_CLUSTER(hsv)
 
 // QWERTY,
 // Light on inner column and underglow
@@ -375,7 +368,7 @@ const rgblight_segment_t* const PROGMEM my_rgb_layers[] = RGBLIGHT_LAYERS_LIST(
 
 layer_state_t layer_state_set_user(layer_state_t state) {
 	rgblight_set_layer_state(0, layer_state_cmp(state, _DEFAULTS) && layer_state_cmp(default_layer_state,_QWERTY));
-	rgblight_set_layer_state(7, layer_state_cmp(state, _DEFAULTS) && layer_state_cmp(default_layer_state,_COLEMAKDH));
+	rgblight_set_layer_state(6, layer_state_cmp(state, _DEFAULTS) && layer_state_cmp(default_layer_state,_COLEMAKDH));
 
 
 	rgblight_set_layer_state(1, layer_state_cmp(state, _LOWER));
@@ -389,10 +382,10 @@ void keyboard_post_init_user(void) {
     // Enable the LED layers
     rgblight_layers = my_rgb_layers;
 
-	rgblight_mode(10);// haven't found a way to set this in a more useful way
+	rgblight_mode(10);// haven't found a way to set this in a more useful way, breathing effect
 
 }
-#endif
+#endif // RGBLIGHT_ENABLE
 
 #ifdef OLED_ENABLE
 
